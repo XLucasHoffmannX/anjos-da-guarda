0dd96cdb6fb51829f65105b07ee6b60769116a6a
hoffmann <henriquelucashoffamnn@gmail.com>
2022-10-10 18:13:33 -0300

    patient migration

---
+++ angels_api/database/migrations/2022_10_10_204608_create_patients_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cpf');
+            $table->string('rg');
+            $table->string('address');
+            $table->string('description');
+            $table->string('reason');
+            $table->string('habits');
+            $table->string('health_problems');
+            $table->string('culture');
+            $table->string('routine_call');
+            $table->string('systemic_drug');
+            $table->string('auto_caution');
+            $table->string('financial');
+            $table->boolean('activated')->default(1);
+            $table->integer('status_m')->default(0);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
